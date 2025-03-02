254c5c64783f5f21af536ec7f528886237b834a5
uzz-ptl7 <uzairpatel7117@gmail.com>
2025-03-02 16:58:15 +0200

    diff version of node.js

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ProfileCard from "./profileCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="app-container">
       <h1>Hello, Uzair! Warm Welcome to React.</h1>
-    </>
-  )
+      <ProfileCard />
+    </div>
+  );
 }
 
-export default App
+export default App;
